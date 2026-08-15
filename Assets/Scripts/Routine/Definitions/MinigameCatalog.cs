c9f5bdedaaa751f371604c769bc83348d4d48b97
GongJaCh <81235791+ghostlink77@users.noreply.github.com>
2026-08-15 20:50:33 +0900

    Feat: Add routine minigame selection
    일과 구간별 미니게임 정의와 셔플백 선정 기반을 추가
    
    - 미니게임 프리팹, 상점, 등장 구간, 심사용 여부 데이터화
    - 계급 구간별 일반 미니게임 후보 필터링
    - 후보 소진 시 재셔플하는 일과 미니게임 선정 규칙 추가

---
+++ Assets/Scripts/Routine/Definitions/MinigameCatalog.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DreamOfMilitary.Routine
+{
+    [CreateAssetMenu(
+        fileName = "MinigameCatalog",
+        menuName = "Dream Of Military/Minigame Catalog")]
+    public sealed class MinigameCatalog : ScriptableObject
+    {
+        [SerializeField]
+        private List<MinigameDef> _definitions =
+            new List<MinigameDef>();
+
+        public IReadOnlyList<MinigameDef> Definitions =>
+            _definitions;
+    }
+}
